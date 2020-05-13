770f4e019d237b0e2752a2bd04e6a46c4cbb72b2
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-13 15:37:12 -0400

    made sidebar and imported dependencies

---
+++ portfolio/src/components/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/core/Menu';
+import ChevronLeftIcon from '@material-ui/core/ChevronLeft';
+import ChevronRightIcon from '@material-ui/core/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/core/MoveToInbox';
+import MailIcon from '@material-ui/core/Mail';
+
