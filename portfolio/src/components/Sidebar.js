426c1f559c87e981c859f3db10bf95ce7a424683
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-13 16:42:18 -0400

    made a sidenav

--- portfolio/src/components/Sidebar.js
+++ portfolio/src/components/Sidebar.js
@@ -10,13 +10,13 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/core/Menu';
-import ChevronLeftIcon from '@material-ui/core/ChevronLeft';
-import ChevronRightIcon from '@material-ui/core/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/core/MoveToInbox';
-import MailIcon from '@material-ui/core/Mail';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 const drawerWidth = 240;
 
