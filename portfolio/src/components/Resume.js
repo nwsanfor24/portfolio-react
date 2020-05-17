83d02f89f5ce092478988b830c6aea17bd15ef09
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-17 18:44:50 -0400

    installed react router dom, making resume component

---
+++ portfolio/src/components/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Box } from "@material-ui/core";
+import Navbar from "./Navbar";
+
+const Resume = () => {
+    return (
+        <Navbar />
+    )
+}
+
+export default Resume
