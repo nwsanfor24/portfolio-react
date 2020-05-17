cf31009dbb8fd310ae75ac975427ef7d09517c11
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-17 19:41:14 -0400

    made a resume page

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -3,10 +3,169 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Box } from "@material-ui/core";
 import Navbar from "./Navbar";
 
+const useStyles = makeStyles(theme => ({
+    mainContainer: {
+        background: "#233"
+    },
+    timeLine: {
+        position: "relative",
+        padding: "1rem",
+        margin: "0 auto",
+        "&:before": {
+            content: "''",
+            position: "absolute",
+            height: "100%",
+            border: "1px solid tan",
+            right: "40px",
+            top: 0
+        },
+        "&:after": {
+            content: "''",
+            display: "table",
+            clear: "both"
+        },
+        [theme.breakpoints.up("md")]: {
+            padding: "2rem",
+            "&:before": {
+                left: "calc(50% - 1px)",
+                right: "auto"
+            }
+        }
+    },
+    timeLineItem: {
+        padding: "1rem",
+        borderBottom: "2px solid tan",
+        position: "relative",
+        margin: "1rem 3rem 1rem 1rem",
+        clear: "both",
+        "&:after": {
+            content: "''",
+            position: "absolute",
+        },
+        "&:before": {
+            content: "''",
+            position: "absolute",
+            right: "-0.625rem",
+            top: "calc(50% - 5px)",
+            borderStyle: "solid",
+            borderColor: "tomato tomato transparent transparent",
+            borderWidth: "0.625rem",
+            transform: "rotate(45deg)",
+        },
+        [theme.breakpoints.up("md")]: {
+            width: "44%",
+            margin: "1rem",
+            "&:nth-of-type(2n)": {
+                float: "right",
+                margin: "1rem",
+                borderColor: "tan"
+            },
+            "&:nth-of-type(2n):before": {
+                right: "auto",
+                left: "-0.625rem",
+                borderColor: "transparent transparent tomato tomato"
+            }
+        }
+    },
+    timeLineYear: {
+        textAlign: "center",
+        maxWidth: "9.375rem",
+        margin: "0 3rem 0 auto",
+        fontSize: "1.8rem",
+        background: "tomato",
+        color: "white",
+        lineHeight: 1,
+        padding: "0.5rem 0 1rem",
+        "&:before": {
+            display: "none"
+        },
+        [theme.breakpoints.up("md")]: {
+            textAlign: "center",
+            margin: "0 auto",
+            "&:nth-of-type(2n)": {
+                float: "none",
+                margin: "0 auto"
+            },
+            "&:nth-of-type(2n):before": {
+                display: "none"
+            }
+        }
+    },
+    heading: {
+        color: "tomato",
+        padding: "3rem 0",
+        textTransform: "uppercase"
+    },
+    subHeading: {
+        color: "white",
+        padding: "0",
+        textTransform: "uppercase"
+    }
+}));
+
 const Resume = () => {
+
+    const classes = useStyles();
+
     return (
-        <Navbar />
-    )
-}
+        <>
+            <Navbar />
+            <Box component="header" className={classes.mainContainer}>
+                <Typography variant="h4" align="center" className={classes.heading}>
+                    Working Experience
+            </Typography>
+                <Box component="div" className={classes.timeLine}>
+                    <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+                        2018
+                </Typography>
+                    <Box component="div" className={classes.timeLineItem}>
+                        <Typography variant="h5" align="center" className={classes.subHeading}>
+                            Production Assistant
+                    </Typography>
+                        <Typography variant="body1" align="center" style={{ color: "tomato" }}>
+                            House Hunters, House Hunters: Renovation, Very Cavallari, The Real World
+                    </Typography>
+                        <Typography variant="subtitle1" align="center" style={{ color: "tan" }}>
+                            Assisted field crews with rigging lights, handling petty cash, organizing lunches,
+                            assisting camera when needed and other production needs
+                    </Typography>
+                    </Box>
+                </Box>
+                <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+                    2019
+                </Typography>
+                <Box component="div" className={classes.timeLineItem}>
+                    <Typography variant="h5" align="center" className={classes.subHeading}>
+                        Production Coordinator
+                    </Typography>
+                    <Typography variant="body1" align="center" style={{ color: "tomato" }}>
+                        Very Cavallari, Racing Wives
+                    </Typography>
+                    <Typography variant="subtitle1" align="center" style={{ color: "tan" }}>
+                        Responsible for making sure that every aspect of the shoot ran smoothly whether
+                        that was a coffee order, getting patrons and a club "released", handling on set logistics
+                        for travel and much more.
+                    </Typography>
+                </Box>
+                <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+                    2020
+                </Typography>
+                <Box component="div" className={classes.timeLineItem}>
+                    <Typography variant="h5" align="center" className={classes.subHeading}>
+                        Full-Stack Bootcamp Student
+                    </Typography>
+                    <Typography variant="body1" align="center" style={{ color: "tomato" }}>
+                        UNH Coding Bootcamp
+                    </Typography>
+                    <Typography variant="subtitle1" align="center" style={{ color: "tan" }}>
+                        Enrolled in a full-stack web development bootcamp to transition from on-set
+                        work to the tech industry. We were taught the basics of HTML and CSS as well as the
+                        MERN Stack, ReactJS, Bootstrap, Git, and much more.
+                    </Typography>
+                </Box>
+            </Box>
+        </>
+    );
+};
 
 export default Resume
