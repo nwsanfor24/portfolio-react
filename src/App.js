740b3911dbdb511aa9adf6e6275598a77c7d9189
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-18 13:44:40 -0400

    moved everything out of the folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import "../src/App.css";
+import "./App.css";
 import Home from './components/index';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Resume from './components/Resume';
