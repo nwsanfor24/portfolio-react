e3347ad9cced01d0c28819cd1c79f0c2022bc8e2
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-18 18:10:12 -0400

    Updates

---
+++ precache-manifest.a3e5325da6d87a0dc09113cbb7c8d991.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daea62c3f6480db8bd634f3b40af1605",
+    "url": "/portfolio-react//index.html"
+  },
+  {
+    "revision": "f8bee0e06f3fb5ce815b",
+    "url": "/portfolio-react//static/css/main.cbd1ae02.chunk.css"
+  },
+  {
+    "revision": "cfd6f8973c2854730fb8",
+    "url": "/portfolio-react//static/js/2.84eea2e1.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio-react//static/js/2.84eea2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8bee0e06f3fb5ce815b",
+    "url": "/portfolio-react//static/js/main.5134e2c7.chunk.js"
+  },
+  {
+    "revision": "713026de090172d7ffd3",
+    "url": "/portfolio-react//static/js/runtime-main.a5c941df.js"
+  },
+  {
+    "revision": "825ce860fc936bbad9f72d18c25c0f12",
+    "url": "/portfolio-react//static/media/Letty.825ce860.png"
+  },
+  {
+    "revision": "857356f562fb5ffb89eec249ba99a8aa",
+    "url": "/portfolio-react//static/media/Project1.857356f5.png"
+  },
+  {
+    "revision": "a5f34bf66f80b936b9dde3d975529266",
+    "url": "/portfolio-react//static/media/Project2.a5f34bf6.png"
+  },
+  {
+    "revision": "42221492ba0d11fea2a7651a7653495a",
+    "url": "/portfolio-react//static/media/bubble.42221492.jpg"
+  },
+  {
+    "revision": "7c6da0ad54f882383ab0a742307dbd99",
+    "url": "/portfolio-react//static/media/computer-desk.7c6da0ad.jpg"
+  },
+  {
+    "revision": "bed4087e80aa2b1e67a34c3fb559f0c1",
+    "url": "/portfolio-react//static/media/unnamed.bed4087e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-react/precache-manifest.de650fe1fd1015a23aa98c5f95d2ce64.js"
+  "/portfolio-react//precache-manifest.a3e5325da6d87a0dc09113cbb7c8d991.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-react//index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
