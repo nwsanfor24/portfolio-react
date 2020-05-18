511a167d5835f51806d875fe443b5423d832559e
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-18 18:15:49 -0400

    Updates

---
+++ precache-manifest.202d8273ef12c9509fd805cc5d735666.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2c21dea8408bca523647ca050828fc",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "31f630ef502cd28b7268",
+    "url": "/portfolio-react/static/css/main.ff309b76.chunk.css"
+  },
+  {
+    "revision": "cfd6f8973c2854730fb8",
+    "url": "/portfolio-react/static/js/2.84eea2e1.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio-react/static/js/2.84eea2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f630ef502cd28b7268",
+    "url": "/portfolio-react/static/js/main.2856967c.chunk.js"
+  },
+  {
+    "revision": "c0ac4d1bfb875ce412bd",
+    "url": "/portfolio-react/static/js/runtime-main.60521b9c.js"
+  },
+  {
+    "revision": "825ce860fc936bbad9f72d18c25c0f12",
+    "url": "/portfolio-react/static/media/Letty.825ce860.png"
+  },
+  {
+    "revision": "857356f562fb5ffb89eec249ba99a8aa",
+    "url": "/portfolio-react/static/media/Project1.857356f5.png"
+  },
+  {
+    "revision": "a5f34bf66f80b936b9dde3d975529266",
+    "url": "/portfolio-react/static/media/Project2.a5f34bf6.png"
+  },
+  {
+    "revision": "42221492ba0d11fea2a7651a7653495a",
+    "url": "/portfolio-react/static/media/bubble.42221492.jpg"
+  },
+  {
+    "revision": "7c6da0ad54f882383ab0a742307dbd99",
+    "url": "/portfolio-react/static/media/computer-desk.7c6da0ad.jpg"
+  },
+  {
+    "revision": "bed4087e80aa2b1e67a34c3fb559f0c1",
+    "url": "/portfolio-react/static/media/unnamed.bed4087e.jpg"
+  }
+]);
