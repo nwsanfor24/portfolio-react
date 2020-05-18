a5881a26759bed7c735db719114a49b454e4ba6e
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-17 21:57:42 -0400

    made a contacts component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/index';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
+import Contacts from './components/Contacts';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <Route exact path="/" component={Home}/>
     <Route path="/resume" component={Resume}/>
     <Route path="/portfolio" component={Portfolio} />
+    <Route path="/contacts" component={Contacts} />
   </>
   );
 }
---
+++ portfolio/src/components/Contacts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Typography, Button, Grid, Box } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import Navbar from "./Navbar";
+
+const Contacts = () => {
+    return (
+        <div>
+            This is the contacts page
+        </div>
+    );
+};
+
+export default Contacts
