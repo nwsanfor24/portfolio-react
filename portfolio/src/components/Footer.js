dcfebe52a8f50cbf4ad16ab3d232ad82ef8baf7d
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-18 11:28:30 -0400

    made footer, imported into navbar but it seems weird

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
-import Facebook from "@material-ui/icons";
+import Facebook from "@material-ui/icons/Facebook";
+import LinkedIn from "@material-ui/icons/LinkedIn";
+import Instagram from "@material-ui/icons/Instagram";
+import GitHub from "@material-ui/icons/GitHub";
+
+const useStyles = makeStyles({
+    root: {
+        "& .MuiBottomNavigationAction-root": {
+            minWidth: 0,
+            maxWidth: 250,
+        },
+    },
+});
+
 
 const Footer = () => {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            
-        </div>
+        <BottomNavigation width="auto" style={{ background: "#222" }}>
+            <BottomNavigationAction
+                className={classes.root}
+                style={{ padding: 0 }}
+                icon={<Facebook />}
+            />
+            <BottomNavigationAction
+                className={classes.root}
+                style={{ padding: 0 }}
+                icon={<Instagram />}
+            />
+            <BottomNavigationAction
+                className={classes.root}
+                style={{ padding: 0 }}
+                icon={<LinkedIn />}
+            />
+            <BottomNavigationAction
+                className={classes.root}
+                style={{ padding: 0 }}
+                icon={<GitHub />}
+            />
+        </BottomNavigation>
     )
 }
 
